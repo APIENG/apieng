b3da530c202d4162ad3d48e98d0a773a32d5efb3
musoye <oyebamijimustapha44@gmail.com>
2024-08-11 01:15:03 +0100

    feat: further revamp

--- internal/models/metrics.go
+++ internal/models/metrics.go
@@ -2,14 +2,7 @@ package models
 
 import (
 	"time"
-	"golang.org/x/crypto/bcrypt"
 )
-
-
-type Users struct {
-	Email    string
-	Password string
-}
 
 // Metrics represents the data structure for storing API metrics.
 type Metrics struct {
@@ -19,19 +12,4 @@ type Metrics struct {
 	ResponseTime      time.Duration
 	Timestamp         time.Time
 	EnergyConsumption float64
-}
-
-func (u *Users) SetPassword(password string) error {
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		return err
-	}
-	u.Password = string(hashedPassword)
-	return nil
-}
-
-
-func (u *Users) CheckPassword(password string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
-	return err == nil
 }
