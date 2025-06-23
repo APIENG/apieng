1a8ee1af1f0071f90a3df0c84ee2948d1d4928b6
AbrahamAlgorithm <abrahamfolorunso6@gmail.com>
2025-06-23 18:45:46 +0100

    feat: implemented a popup modal to show once we measure an api, and also designed a page for each endpoint

--- internal/models/metrics.go
+++ internal/models/metrics.go
@@ -6,14 +6,15 @@ import (
 
 // Metrics represents the data structure for storing API metrics.
 type Metrics struct {
-	APIEndpoint       string
-	UserId            string
-	Method            string
-	Status            int
-	RequestSize       int
-	ResponseSize      int
-	ResponseTime      time.Duration
-	Timestamp         time.Time
-	EnergyConsumption float64
-	Explanation       string
+	ID                int           `json:"id" db:"id"`
+	APIEndpoint       string        `json:"api_endpoint" db:"api_endpoint"`
+	UserId            string        `json:"user_id" db:"user_id"`
+	Method            string        `json:"method" db:"method"`
+	Status            int           `json:"status" db:"status"`
+	RequestSize       int           `json:"request_size" db:"request_size"`
+	ResponseSize      int           `json:"response_size" db:"response_size"`
+	ResponseTime      time.Duration `json:"response_time" db:"response_time"`
+	Timestamp         time.Time     `json:"timestamp" db:"timestamp"`
+	EnergyConsumption float64       `json:"energy_consumption" db:"energy_consumption"`
+	Explanation       string        `json:"explanation" db:"explanation"`
 }
